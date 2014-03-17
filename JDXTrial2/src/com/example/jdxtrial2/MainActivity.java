1f8b41042c183f6e5a1719303d998f2220507f99
Jared Hagemann <jdx11088@gmail.com>
2014-03-16 19:28:29 -0500

    Added a log message to main activity

--- JDXTrial2/src/com/example/jdxtrial2/MainActivity.java
+++ JDXTrial2/src/com/example/jdxtrial2/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ConfigurationInfo;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -19,7 +20,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		Log.e("JDX Main Activity", "Second Push");
 		//Set Content View
 		glSurfaceView = new JDXTrial2SurfaceView(this);
 		setContentView(glSurfaceView);
